a167bf1b8386eb21f6648a3a4c5c38b6e6fc8dee
Benwei Xu <benwei_xu@apple.com>
2018-12-01 17:58:47 -0800

    add find overlap date sol

--- tools_db_create.py
+++ tools_db_create.py
@@ -1,4 +1,5 @@
 import sqlite3
+from datetime import date, datetime
 
 conn = sqlite3.connect(':memory:') #create connect obj, can be in memory
 #in memory method each time start fresh, no need comment out create or insert code
@@ -32,10 +33,10 @@ c.execute("INSERT INTO curators VALUES (5,'Rebecca Votea','Esteemed naturalist')
 
 
 #insert exhibits
-c.execute("INSERT INTO exhibits VALUES (3,'Free The Fishes',2018-01-01,2018-06-30,5)")
-c.execute("INSERT INTO exhibits VALUES (17,'Space, What Lies Above',2018-02-01,2018-05-30,11)")
-c.execute("INSERT INTO exhibits VALUES (23,'Bears Bears Bears',2018-02-14,2018-02-24,5)")
-c.execute("INSERT INTO exhibits VALUES (46,'Humans? Aliens?',2019-03-14,2019-10-21,11)")
+c.execute("INSERT INTO exhibits VALUES (3,'Free The Fishes','2018-01-01','2018-06-30',5)")
+c.execute("INSERT INTO exhibits VALUES (17,'Space, What Lies Above','2018-02-01','2018-05-30',11)")
+c.execute("INSERT INTO exhibits VALUES (23,'Bears Bears Bears','2018-02-14','2018-02-24',5)")
+c.execute("INSERT INTO exhibits VALUES (46,'Humans? Aliens?','2019-03-14','2019-10-21',11)")
 
 #select statement i.e. read
 # c.execute("SELECT * FROM exhibits")
@@ -55,11 +56,31 @@ c.execute("INSERT INTO exhibits VALUES (46,'Humans? Aliens?',2019-03-14,2019-10-
 
 #learn different kind of join, right and outer join not support in sqlite
 #c.execute("SELECT c.name, e.name FROM curators c JOIN exhibits e ON c.id=e.curator_id")
-c.execute("SELECT c.name, e.name FROM curators c LEFT JOIN exhibits e ON c.id=e.curator_id")
+# c.execute("SELECT c.name, e.name FROM curators c LEFT JOIN exhibits e ON c.id=e.curator_id")
+# rows=c.fetchall()
+# print(rows)
+
+#c.execute("SELECT c.name, e.name FROM curators c LEFT JOIN exhibits e ON c.id=e.curator_id WHERE c.name LIKE 'R%'")
+# c.execute("SELECT c.name, COUNT(e.name) FROM curators c LEFT JOIN exhibits e ON c.id=e.curator_id WHERE c.name LIKE 'R%' GROUP BY c.name")
+# rows=c.fetchall()
+# print(rows)
+
+#figure out date
+c.execute("SELECT * from exhibits")
+rows=c.fetchall()
+now0=datetime.strptime(rows[0][2],'%Y-%m-%d').date()
+now1=datetime.strptime(rows[1][2],'%Y-%m-%d').date()
+now2=datetime.strptime(rows[2][2],'%Y-%m-%d').date()
+now3=datetime.strptime(rows[3][2],'%Y-%m-%d').date()
+
+#now00=date.strftime('%Y-%m-%d',rows[0][2])
+#now00=datetime(int(rows[0][2]))
+
+#show overlap
+#trick is use e1.id<e2.id to make them unique
+c.execute("SELECT e1.name,e2.name from exhibits e1, exhibits e2 WHERE e1.start_date<=e2.end_date and e2.start_date<=e1.end_date and e1.id<e2.id")
 rows=c.fetchall()
 print(rows)
-
-
 
 conn.commit() #commit to submit current change
 conn.close()
